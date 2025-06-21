34135f032fbee226d68b39d524c353e8ed39ce35
NITINCHALOPADHO <nitin@chalopadho.com>
2025-06-21 19:13:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Feed from "./components/homePage/Feedback"
 import HomePage from "./components/homePage/HomePage";
 import ScrollArrow from "./Scroll.jsx"
 function App() {
+
   return (
     <div className="App">
       <HomePage />
